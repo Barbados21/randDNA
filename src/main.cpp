2dad13a1a11d5a3e4ad62baba08938a9aeb078ca
Barbados21 <Barbados21@users.noreply.github.com>
2019-02-13 11:58:17 -0400

    Lab 2

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,9 @@ int main()
 
     cout<<"\nWelcome to the DNA generator\n\n\tPlease enter a seed, the letter bases to be used (e.g. TGAC) \n\tand the length of the DNA sequence to be generated\n\n\t>>> ";   
 
-    cin>>seed>>bases>>n;//read the three inputs
+    cin>>seed;
+    cin>>bases;
+    cin>>n;//read the three inputs
 
     cout <<"\n\nOutput:\n\t"<<randDNA(seed,bases,n)<<endl; //print output  
 }
--- src/rdna.hpp
+++ src/rdna.hpp
@@ -6,14 +6,15 @@ using namespace std;
 
 string randDNA(int seed,string bases,int n)
 {
+	int RandNumber;
 	string outcome;
 	int min = 0, i = 0;
 	mt19937 eng1(seed);
 	uniform_int_distribution<> uniform(min, bases.size() - 1);
 	while(i < n)
 	{
-		random = uniform(eng1);
-		outcome += bases[random];
+		RandNumber = uniform(eng1);
+		outcome += bases[RandNumber];
 	}
 	return outcome;
 }
