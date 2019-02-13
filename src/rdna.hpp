91a9686978188bf1dc89914969997ee0e1ba7ab3
Barbados21 <Barbados21@users.noreply.github.com>
2019-02-13 12:06:28 -0400

    Lab2

--- src/rdna.hpp
+++ src/rdna.hpp
@@ -10,7 +10,7 @@ string randDNA(int seed,string bases,int n)
 	string outcome;
 	int min = 0, i = 0;
 	mt19937 eng1(seed);
-	uniform_int_distribution<> uniform(min, bases.size() - 1);
+	uniform_int_distribution<int> uniform(min, bases.size() - 1);
 	while(i < n)
 	{
 		RandNumber = uniform(eng1);
